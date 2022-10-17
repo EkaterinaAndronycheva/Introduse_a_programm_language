5b3c410e9c8b26c2d3f5a215bf8d59427c20cd24
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-10-17 19:17:05 +0300

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Программа выводит третью цифру заданного числа.");
+            Console.WriteLine("Введите число: ");
+            int n;
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Введено некорректое значение");
+            }
+            else
+            {     
+                if (n < 0) n *= -1;           
+                if (n < 100) 
+                {
+                    Console.WriteLine("Третьей цифры нет");
+                }
+                else
+                {
+                    while (n > 999)
+                    {
+                        n /= 10;
+                    }
+                Console.WriteLine(n%10);
+                }              
+            }
+            
+        }
+    }
+}
