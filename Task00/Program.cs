64209d64aa4fbd7bce5acd689de93d049d5ecf3f
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-10-17 13:42:51 +0300

    Add ready.1 Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -4,9 +4,12 @@ namespace Task00
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int square = number * number;
+            Console.WriteLine($"Квадрат числа {number} = {square}");
         }
     }
 }
