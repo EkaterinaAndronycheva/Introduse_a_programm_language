2300d60db1f194cc30e27ce5326cbb1a8d5ccd3a
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-10-17 14:15:31 +0300

    Add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+///а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+using System;
+
+namespace Task08
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Программа выводит все четные числа от 1 до N.");
+            Console.Write("Введите число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 1)
+            {
+                Console.WriteLine("Введено некорректное число.");
+            }
+            else
+            {
+                Console.Write("1 ");
+                for (int i = 2; i <= n; i+=2)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
