4445f5c19c66d49887fe2e0bd5eddcb2b631a185
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-10-17 19:30:03 +0300

    Add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+using System;
+
+namespace Task15
+{
+    class Program
+    {
+        static void PrintIsHoliday (int day)
+        {
+            if (day == 1) Console.WriteLine("Это не выходной день");
+            if (day == 2) Console.WriteLine("Это не выходной день");
+            if (day == 3) Console.WriteLine("Это не выходной день");
+            if (day == 4) Console.WriteLine("Это не выходной день");
+            if (day == 5) Console.WriteLine("Это не выходной день");
+            if (day == 6) Console.WriteLine("Это выходной день");
+            if (day == 7) Console.WriteLine("Это выходной день");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите цифру, обозначающую день недели: ");
+            int n;
+            if (int.TryParse(Console.ReadLine(), out n))
+            {
+                if (n > 9 || n < 1) Console.WriteLine("Введено некорректное значение");
+                else
+                {
+                    PrintIsHoliday(n);
+                }
+            }
+            else Console.WriteLine("Введено некорректное значение");
+        }
+    }
+}
