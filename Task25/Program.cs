e38ef4dd718b874d00a5b31bd0c5fccf572b1451
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-11-09 18:35:45 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System;
+
+namespace Task25
+{
+    class Program
+    {
+        static double PowN (double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+        static void Main()
+        {
+            Console.WriteLine("Программа возводит число А в натуральную степень B");
+            Console.Write("Введите число А: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{a}, {b} -> {PowN(a, b)}");
+        }
+    }
+}
