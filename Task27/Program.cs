3080b01ade1a8ac05fe1a8ca3687d8921e50672a
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-11-09 18:54:45 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;
+
+namespace Task27
+{
+    class Program
+    {
+        static int SumDigits (int x)
+        {
+            x = Math.Abs(x);
+            int sum = default;
+            while (x > 0)
+            {
+                sum += x%10;
+                x /= 10;
+            }
+            return sum;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Программа выводит сумму цифр числа.");
+            Console.Write("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{a} -> {SumDigits(a)}");
+        }
+    }
+}
