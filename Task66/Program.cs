7b024cb20c9da2b7672d826fc0ca97c5254a9544
Ekaterina_Andronycheva <katerina.andronycheva@yandex.ru>
2022-11-27 17:59:31 +0300

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+
+namespace Task66
+{
+    class Program
+    {
+        static int Sum (int m, int n)
+        {
+            if (n == m) return n;
+            else return m > n ? n + Sum(m, n+1) : m + Sum(n, m+1);         
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите натуральное число M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите натуральное число N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма = {Sum(m, n)}");
+        }
+    }
+}
